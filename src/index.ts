0d5b0f2d94ec86b5c462c97ef883a72cae6fc684
autopp <autopp.inc@gmail.com>
2021-08-05 01:44:15 +0900

    feat: add assoc

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
-export function clear<T>(x: Array<T>): Array<T> {
-  x.length = 0
-  return x
+export function clear<T>(array: Array<T>): Array<T> {
+  array.length = 0
+  return array
+}
+
+export function assoc<K, R extends unknown[]>(array: [K, ...R][], key: K): [K, ...R] | undefined {
+  return array.find(([k]) => k === key)
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,10 +1,25 @@
-import * as arubyray from '../src'
+import * as Arubyray from '../src'
 
 describe('clear()', () => {
   it('clears the given array and returns it', () => {
-    const arr = [1, 2, 3]
+    const array = [1, 2, 3]
 
-    expect(arubyray.clear(arr)).toBe(arr)
-    expect(arr).toEqual([])
+    expect(Arubyray.clear(array)).toBe(array)
+    expect(array).toEqual([])
+  })
+})
+
+describe('assoc()', () => {
+  const array = [
+    [1, 'a'],
+    [2, 'b', 'c'],
+    [3, 'd', 'e', 'f'],
+  ] as [number, ...string[]][]
+
+  it.each([
+    ['when matching tupple is found, returns it', 2, [2, 'b', 'c']],
+    ['when matching tuple is not found, returns undefined', 4, undefined],
+  ])('%s', (_, key, expected) => {
+    expect(Arubyray.assoc(array, key)).toEqual(expected)
   })
 })
