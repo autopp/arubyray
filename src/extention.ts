b7f1a861e58c675f73f56bd425c8ed196afacedb
autopp <autopp.inc@gmail.com>
2021-08-08 17:10:41 +0900

    refactor: reduce cast by using this parameter

--- src/extention.ts
+++ src/extention.ts
@@ -20,53 +20,54 @@ declare global {
   }
 }
 
-Array.prototype.clear = function <T>(): T[] {
-  return Arubyray.clear(this as T[])
+Array.prototype.clear = function <T>(this: T[]): T[] {
+  return Arubyray.clear(this)
 }
 
-Array.prototype.compact = function <T>(): T[] {
-  return Arubyray.compact(this as T[])
+Array.prototype.compact = function <T>(this: T[]): T[] {
+  return Arubyray.compact(this)
 }
 
-Array.prototype.combination = function <T>(n: number): T[][] {
-  return Arubyray.combination(this as T[], n)
+Array.prototype.combination = function <T>(this: T[], n: number): T[][] {
+  return Arubyray.combination(this, n)
 }
 
-Array.prototype.count = function <T>(callbackfn: (x: T) => boolean): number {
-  return Arubyray.count(this as T[], callbackfn)
+Array.prototype.count = function <T>(this: T[], callbackfn: (x: T) => boolean): number {
+  return Arubyray.count(this, callbackfn)
 }
 
-Array.prototype.drop = function <T>(n: number): T[] {
-  return Arubyray.drop(this as T[], n)
+Array.prototype.drop = function <T>(this: T[], n: number): T[] {
+  return Arubyray.drop(this, n)
 }
 
-Array.prototype.dropWhile = function <T>(callbackfn: (x: T) => boolean): T[] {
-  return Arubyray.dropWhile(this as T[], callbackfn)
+Array.prototype.dropWhile = function <T>(this: T[], callbackfn: (x: T) => boolean): T[] {
+  return Arubyray.dropWhile(this, callbackfn)
 }
 
-Array.prototype.permutation = function <T>(n: number): T[][] {
-  return Arubyray.permutation(this as T[], n)
+Array.prototype.permutation = function <T>(this: T[], n: number): T[][] {
+  return Arubyray.permutation(this, n)
 }
 
 Array.prototype.product = function <T, U, W extends readonly unknown[]>(
+  this: T[],
   other?: readonly U[],
   ...list: TupleToTupleOfArray<W>
 ): T[][] | [T, U, ...W][] {
   if (other === undefined) {
-    return Arubyray.product(this as T[])
+    return Arubyray.product(this)
   }
 
-  return Arubyray.product<T, U, W>(this as T[], other, ...list)
+  return Arubyray.product<T, U, W>(this, other, ...list)
 }
 
-Array.prototype.repeatedCombination = function <T>(n: number): T[][] {
-  return Arubyray.repeatedCombination(this as T[], n)
+Array.prototype.repeatedCombination = function <T>(this: T[], n: number): T[][] {
+  return Arubyray.repeatedCombination(this, n)
 }
 
-Array.prototype.repeatedPermutation = function <T>(n: number): T[][] {
-  return Arubyray.repeatedPermutation(this as T[], n)
+Array.prototype.repeatedPermutation = function <T>(this: T[], n: number): T[][] {
+  return Arubyray.repeatedPermutation(this, n)
 }
 
-Array.prototype.take = function <T>(n: number): T[] {
-  return Arubyray.take(this as T[], n)
+Array.prototype.take = function <T>(this: T[], n: number): T[] {
+  return Arubyray.take(this, n)
 }
